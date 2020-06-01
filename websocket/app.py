c0654b832a6d4891de183612f731070b02d98bce
Antonio Tripiana Caballero <tonitripiana@gmail.com>
2020-06-01 18:06:13 +0200

    first socketio

--- websocket/app.py
+++ websocket/app.py
@@ -6,13 +6,16 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def session():
+    return render_template('session.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
 
 @socketio.on('my event')
-def test_message(message):
-    print(message)
-    emit('my response', {'data': 'got it!'})
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
     socketio.run(app)
